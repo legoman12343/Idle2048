3f3d6fdf9298800f5b73a0cbc583a58d79894849
aemeny <104924052+aemeny@users.noreply.github.com>
2022-05-04 23:26:51 +0100

    df

--- Idle 2048/Assets/C#/Shop Scripts/ShopButtonPopUp.cs
+++ Idle 2048/Assets/C#/Shop Scripts/ShopButtonPopUp.cs
@@ -4,25 +4,70 @@ using UnityEngine;
 
 public class ShopButtonPopUp : MonoBehaviour
 {
-    public GameObject ShopTab;
-    bool activeShopTab;
+    public GameObject UpgradeTab;
+    public GameObject AscensionTab;
+    public GameObject SpecialCurrencyTab;
+    public GameObject ExtraAreaTab;
+    int activeShopTab;
 
     void Start()
     {
-        activeShopTab = false;
+        activeShopTab = 1;
     }
 
-    public void OpenShopTab()
+    void changeShopTab()
     {
-        if (activeShopTab == false)
+        if (activeShopTab == 1)
         {
-            ShopTab.SetActive(true);
-            activeShopTab = true;
+            UpgradeTab.SetActive(true);
+            AscensionTab.SetActive(false);
+            SpecialCurrencyTab.SetActive(false);
+            ExtraAreaTab.SetActive(false);
         }
-        else
+        else if (activeShopTab == 2)
         {
-            ShopTab.SetActive(false);
-            activeShopTab = false;
+            UpgradeTab.SetActive(false);
+            AscensionTab.SetActive(true);
+            SpecialCurrencyTab.SetActive(false);
+            ExtraAreaTab.SetActive(false);
         }
+        else if (activeShopTab == 3)
+        {
+            UpgradeTab.SetActive(false);
+            AscensionTab.SetActive(false);
+            SpecialCurrencyTab.SetActive(true);
+            ExtraAreaTab.SetActive(false);
+        }
+        else if (activeShopTab == 4)
+        {
+            UpgradeTab.SetActive(false);
+            AscensionTab.SetActive(false);
+            SpecialCurrencyTab.SetActive(false);
+            ExtraAreaTab.SetActive(true);
+        }
+    }
+
+    public void OpenUpgradeTab()
+    {
+        activeShopTab = 1;
+        changeShopTab();
+    }
+
+    public void OpenAscensionTab()
+    {
+        activeShopTab = 2;
+        changeShopTab();
+    }
+
+    public void OpenSpecialCurrencyTab()
+    {
+        activeShopTab = 3;
+        changeShopTab();
+    }
+
+    public void OpenExtrasTab()
+    {
+        activeShopTab = 4;
+        changeShopTab();
     }
 }
