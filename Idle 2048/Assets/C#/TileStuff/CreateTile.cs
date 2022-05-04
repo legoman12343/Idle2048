30eb0e94251be8e45b25f0f1101c1057f927017f
legoman12343 <joshua.markey@outlook.com>
2022-05-04 23:22:25 +0100

    Tiles

--- Idle 2048/Assets/C#/TileStuff/CreateTile.cs
+++ Idle 2048/Assets/C#/TileStuff/CreateTile.cs
@@ -50,22 +50,25 @@ public class CreateTile : MonoBehaviour
     {
         int random = 1;
         bool check = false;
-        random = Random.Range(0, 16);
-        RaycastHit2D hit = Physics2D.Raycast(tileTransform[random].position, Vector2.up, 0.01f);
-        if (hit.collider != null)
-        {
-            check = true;
-            Debug.Log("HIT");
-        }else
-        {
-            Debug.Log("MISS");
-        }
-        /*
+        
         while (check == false)
         {
+            random = Random.Range(0, 16);
+            RaycastHit2D hit = Physics2D.Raycast(tileTransform[random].position, Vector2.up, 0.001f);
+            if (hit.collider != null)
+            {
+                Debug.Log("HIT");
+            }
+            else
+            {
+                check = true;
+                Debug.Log("MISS");
+            }
+        
+        
             
         }
-        */
+        
         Instantiate(tile, tileTransform[random].position, tileTransform[random].rotation);
     }
 }
