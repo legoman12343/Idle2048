99b51e2ba9c7d7125500d0532e2db09c673f42a6
legoman12343 <joshua.markey@outlook.com>
2022-05-08 15:15:19 +0100

    v

--- Idle 2048/Assets/C#/Monsters/MonsterPrefabStuff.cs
+++ Idle 2048/Assets/C#/Monsters/MonsterPrefabStuff.cs
@@ -6,12 +6,14 @@ using Assets.FantasyMonsters.Scripts;
 
 public class MonsterPrefabStuff : MonoBehaviour
 {
+    
     public List<GameObject> monsterPrefabList = new List<GameObject>();
     public List<GameObject> bossPrefabList = new List<GameObject>();
     public LevelController level;
     public Transform monsterSpawnPoint;
     private GameObject[] monster;
     public HealthBarScript healthBar;
+    //coin splash
     public GameObject coinPrefab;
     public CoinsDisplay moneyScript;
     public int multiplier;
@@ -20,11 +22,13 @@ public class MonsterPrefabStuff : MonoBehaviour
     {
         monster = new GameObject[1];
         spawnMonster();
+        //coin multiplier for crates
         multiplier = 1;
     }
 
     public void spawnMonster()
     {
+        //spawn boss
         if(level.level % 10 == 0 && level.level > 9)
         {
             int prefabIndex = UnityEngine.Random.Range(0, bossPrefabList.Count - 1);
@@ -33,13 +37,14 @@ public class MonsterPrefabStuff : MonoBehaviour
             level.requiredKills = 1;
         }
         else
-        {
+        {//spawn normal monster
             int prefabIndex = UnityEngine.Random.Range(0, monsterPrefabList.Count - 1);
             monster[0] = Instantiate(monsterPrefabList[prefabIndex], monsterSpawnPoint.position, Quaternion.identity);
             monster[0].GetComponent<Stats>().boss = false;
             level.requiredKills = 10;
 
         }
+        //set monster values
         var stats = monster[0].GetComponent<Stats>();
         stats.healthBar = healthBar;
         stats.monsterScript = this;
@@ -47,13 +52,15 @@ public class MonsterPrefabStuff : MonoBehaviour
         healthBar.isDead = false;
         stats.Init();        
     }
-
+    //coroutine for death
     public IEnumerator respawnMonster()
     {
+        
         level.killCount++;
         bool coinsMade = false;
+        //death animation script
         monster[0].GetComponent<Monster>().Die();
-        
+        //make coins splash
         if (coinsMade == false)
         {
             int range = Random.Range(5, 8);
@@ -69,10 +76,11 @@ public class MonsterPrefabStuff : MonoBehaviour
             }
             coinsMade = true;
         }
-        
-        
-        yield return new WaitForSeconds(1.1f);
+        //add coins to total
         moneyScript.addCoins(monster[0].GetComponent<Stats>().coins * multiplier);
+        //wait
+        yield return new WaitForSeconds(1.1f);
+        //destroy obj
         Destroy(monster[0]);
         spawnMonster();
     }
