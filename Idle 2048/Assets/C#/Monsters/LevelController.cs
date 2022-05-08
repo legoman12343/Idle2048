c51148b34ed0a3bbff4cbfc1a84d7dede67273a9
aemeny <104924052+aemeny@users.noreply.github.com>
2022-05-08 16:35:02 +0100

    Level Changer

--- Idle 2048/Assets/C#/Monsters/LevelController.cs
+++ Idle 2048/Assets/C#/Monsters/LevelController.cs
@@ -8,29 +8,68 @@ public class LevelController : MonoBehaviour
     public TextMeshProUGUI levelCount;
     public TextMeshProUGUI killCountText;
     public int level;
+    public int levelMax;
     public int killCount;
     public int requiredKills;
+    public bool ProgressMode;
+    public GameObject forwardsButton;
+    public GameObject backButton;
+    public MonsterPrefabStuff MonsterScript;
     // Start is called before the first frame update
     void Start()
     {
+        ProgressMode = true;
         requiredKills = 10;
         killCount = 0;
         level = 1;
         levelCount.text = level.ToString();
+        killCountText.text = killCount.ToString() + "/" + requiredKills.ToString();
     }
 
     // Update is called once per frame
-    void Update()
+    public void LevelUpdate()
     {
         killCountText.text = killCount.ToString() + "/" + requiredKills.ToString();
-        if (killCount == requiredKills)
+        if (ProgressMode == true)
         {
-            level++;
-            levelCount.text = level.ToString();
-            killCount = 0;
+            if (killCount == requiredKills)
+            {
+                if (level == levelMax) levelMax++;
+
+                level++;
+
+                if (level == levelMax) forwardsButton.SetActive(false);
+
+                levelCount.text = level.ToString();
+
+                if (level == levelMax) killCount = 0;
+            }
         }
     }
 
+    public void progressionMode()
+    {
+        if (ProgressMode) ProgressMode = false;
+        else ProgressMode = true;
+    }
+
+    public void decreaseLevel()
+    {
+        level--;
+        killCount = 10;
+        ProgressMode = false;
+        forwardsButton.SetActive(true);
+        if (level == 1) backButton.SetActive(false);
+        else backButton.SetActive(true);
+        MonsterScript.DecreaseLevel();
+    }
+
+    public void increaseLevel()
+    {
+        level++;
+        killCount = 10;
+        MonsterScript.respawnMonster();
+    }
 
     public int getMonsterHealth()
     {
--- Idle 2048/Assets/C#/Monsters/MonsterPrefabStuff.cs
+++ Idle 2048/Assets/C#/Monsters/MonsterPrefabStuff.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Assets.FantasyMonsters.Scripts;
+using DG.Tweening;
 
 public class MonsterPrefabStuff : MonoBehaviour
 {
@@ -10,16 +11,21 @@ public class MonsterPrefabStuff : MonoBehaviour
     public List<GameObject> monsterPrefabList = new List<GameObject>();
     public List<GameObject> bossPrefabList = new List<GameObject>();
     public LevelController level;
-    public Transform monsterSpawnPoint;
+    public Transform monsterSpawnPointRight;
+    public Transform monsterBattlePoint;
+    public Transform monsterSpawnPointLeft;
     private GameObject[] monster;
     public HealthBarScript healthBar;
     //coin splash
     public GameObject coinPrefab;
     public CoinsDisplay moneyScript;
     public int multiplier;
+    bool backLevel;
+    private Vector3 spawnPoint;
 
     void Start()
     {
+        backLevel = false;
         monster = new GameObject[1];
         spawnMonster();
         //coin multiplier for crates
@@ -28,18 +34,25 @@ public class MonsterPrefabStuff : MonoBehaviour
 
     public void spawnMonster()
     {
+        if (backLevel == true)
+        {
+            spawnPoint = monsterSpawnPointLeft.position;
+            backLevel = false;
+        }
+        else spawnPoint = monsterSpawnPointRight.position;
+
         //spawn boss
-        if(level.level % 10 == 0 && level.level > 9)
+        if (level.level % 10 == 0 && level.level > 9)
         {
             int prefabIndex = UnityEngine.Random.Range(0, bossPrefabList.Count - 1);
-            monster[0] = Instantiate(bossPrefabList[prefabIndex], monsterSpawnPoint.position, Quaternion.identity);
+            monster[0] = Instantiate(bossPrefabList[prefabIndex], spawnPoint, Quaternion.identity);
             monster[0].GetComponent<Stats>().boss = true;
             level.requiredKills = 1;
         }
         else
         {//spawn normal monster
             int prefabIndex = UnityEngine.Random.Range(0, monsterPrefabList.Count - 1);
-            monster[0] = Instantiate(monsterPrefabList[prefabIndex], monsterSpawnPoint.position, Quaternion.identity);
+            monster[0] = Instantiate(monsterPrefabList[prefabIndex], spawnPoint, Quaternion.identity);
             monster[0].GetComponent<Stats>().boss = false;
             level.requiredKills = 10;
 
@@ -50,13 +63,24 @@ public class MonsterPrefabStuff : MonoBehaviour
         stats.monsterScript = this;
         stats.level = level;
         healthBar.isDead = false;
-        stats.Init();        
+        stats.Init();
+        monster[0].transform.DOMove(monsterBattlePoint.position, 1f);
     }
+
+    public IEnumerator DecreaseLevel()
+    {
+        var deadMonster = monster[0];
+        deadMonster.transform.DOMove(monsterSpawnPointRight.position, 1f);
+        spawnMonster();
+        yield return new WaitForSeconds(1f);
+        backLevel = true;
+    }
+
     //coroutine for death
     public IEnumerator respawnMonster()
     {
-        
         level.killCount++;
+        level.LevelUpdate();
         bool coinsMade = false;
         //death animation script
         monster[0].GetComponent<Monster>().Die();
@@ -70,7 +94,7 @@ public class MonsterPrefabStuff : MonoBehaviour
             }
             for (int i = 0; i < range; i++)
             {
-                Vector3 vec = monsterSpawnPoint.position;
+                Vector3 vec = monsterBattlePoint.position;
                 vec.y += 0.5f;
                 Instantiate(coinPrefab, vec, Quaternion.identity);
             }
@@ -80,10 +104,15 @@ public class MonsterPrefabStuff : MonoBehaviour
         moneyScript.addCoins(monster[0].GetComponent<Stats>().coins * multiplier);
         //wait
         yield return new WaitForSeconds(1.1f);
-        //destroy obj
-        Destroy(monster[0]);
+        var deadMonster = monster[0];
+        deadMonster.transform.DOMove(monsterSpawnPointLeft.position, 1f);
         spawnMonster();
+        yield return new WaitForSeconds(1f);
+        //destroy obj
+        Destroy(deadMonster);
+        level.LevelUpdate();
     }
+
 
 }
 
