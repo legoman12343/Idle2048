e6d3018507bba5e756f2e56b8321ee8bd3650d08
aemeny <104924052+aemeny@users.noreply.github.com>
2022-05-09 03:19:49 +0100

    sdf

--- Idle 2048/Assets/C#/Bottom Tab Scripts/ShopButtonPopUp.cs
+++ Idle 2048/Assets/C#/Bottom Tab Scripts/ShopButtonPopUp.cs
@@ -22,9 +22,14 @@ public class ShopButtonPopUp : MonoBehaviour
     public GameObject Ascension;
     public GameObject GemShop;
     public GameObject Extra;
-    int activeShopTab;
-    int activeUpgradePanel;
+    public GameObject UpgradeTitle;
+    public GameObject GearTitle;
+    private int activeShopTab;
+    private int activeUpgradePanel;
+    private int lastButton;
     public Swipe swipe;
+    private Color32 Dark = new Color32(164, 164, 164, 255);
+    private Color32 Light = new Color32(255, 255, 255, 255);
 
     void Start()
     {
@@ -33,171 +38,113 @@ public class ShopButtonPopUp : MonoBehaviour
 
     void changeShopTab()
     {
-        switch (activeShopTab)
+        if (activeShopTab != 5)
         {
-            case 1:
-                if (UpgradeTab.active)
-                {
+            switch (lastButton)
+            {
+                case 1:
                     Shop.GetComponent<Image>().sprite = Close;
                     ButtonsBoarder.SetActive(true);
                     GearPanel.SetActive(false);
                     UpgradePanel.SetActive(false);
                     UpgradeTab.SetActive(false);
                     swipe.canSwipe = true;
-                }
-                else
-                {
-                    Shop.GetComponent<Image>().sprite = Open;
+                    break;
+                case 2:
                     Ascension.GetComponent<Image>().sprite = Close;
+                    ButtonsBoarder.SetActive(true);
+                    AscensionTab.SetActive(false);
+                    swipe.canSwipe = true;
+                    break;
+                case 3:
                     GemShop.GetComponent<Image>().sprite = Close;
+                    ButtonsBoarder.SetActive(true);
+                    SpecialCurrencyTab.SetActive(false);
+                    swipe.canSwipe = true;
+                    break;
+                case 4:
                     Extra.GetComponent<Image>().sprite = Close;
+                    ButtonsBoarder.SetActive(true);
+                    HealthBar.SetActive(true);
+                    ProgessSwitchButton.SetActive(true);
+                    ExtraAreaTab.SetActive(false);
+                    swipe.canSwipe = true;
+                    break;
+                case 6:
                     Quests.GetComponent<Image>().sprite = Close;
+                    ButtonsBoarder.SetActive(true);
+                    QuestTab.SetActive(false);
+                    swipe.canSwipe = true;
+                    break;
+            }
+        }
+        if (activeShopTab != lastButton || activeShopTab == 5)
+        {
+            switch (activeShopTab)
+            {
+                case 1:
+                    Shop.GetComponent<Image>().sprite = Open;
                     swipe.canSwipe = false;
                     UpgradeTab.SetActive(true);
+                    ButtonsBoarder.SetActive(false);
                     if (activeUpgradePanel == 1)
                     {
+                        GearTitle.GetComponent<Image>().color = Dark;
+                        UpgradeTitle.GetComponent<Image>().color = Light;
                         UpgradePanel.SetActive(true);
                     }
                     else
                     {
-                        GearPanel.SetActive(true);                        
+                        UpgradeTitle.GetComponent<Image>().color = Dark;
+                        GearTitle.GetComponent<Image>().color = Light;
+                        GearPanel.SetActive(true);
                     }
-                    ButtonsBoarder.SetActive(false);
-                    QuestTab.SetActive(false);
-                    HealthBar.SetActive(true);
-                    ProgessSwitchButton.SetActive(true);
-                    AscensionTab.SetActive(false);
-                    SpecialCurrencyTab.SetActive(false);
-                    ExtraAreaTab.SetActive(false);
-                }
-                
-                
-                break;
-            case 2:
-                if (AscensionTab.active)
-                {
-                    Ascension.GetComponent<Image>().sprite = Close;
-                    ButtonsBoarder.SetActive(true);
-                    AscensionTab.SetActive(false);
-                    swipe.canSwipe = true;
-                }
-                else
-                {
+                    break;
+                case 2:
                     Ascension.GetComponent<Image>().sprite = Open;
-                    Shop.GetComponent<Image>().sprite = Close;
-                    GemShop.GetComponent<Image>().sprite = Close;
-                    Extra.GetComponent<Image>().sprite = Close;
-                    Quests.GetComponent<Image>().sprite = Close;
                     ButtonsBoarder.SetActive(false);
                     swipe.canSwipe = false;
                     AscensionTab.SetActive(true);
-                    HealthBar.SetActive(true);
-                    ProgessSwitchButton.SetActive(true);
-                    UpgradeTab.SetActive(false);
-                    QuestTab.SetActive(false);
-                    SpecialCurrencyTab.SetActive(false);
-                    ExtraAreaTab.SetActive(false);
-                }
-                
-                break;
-            case 3:
-                if (SpecialCurrencyTab.active)
-                {
-                    GemShop.GetComponent<Image>().sprite = Close;
-                    ButtonsBoarder.SetActive(true);
-                    SpecialCurrencyTab.SetActive(false);
-                    swipe.canSwipe = true;
-                }
-                else
-                {
+                    break;
+                case 3:
                     GemShop.GetComponent<Image>().sprite = Open;
-                    Shop.GetComponent<Image>().sprite = Close;
-                    Ascension.GetComponent<Image>().sprite = Close;
-                    Extra.GetComponent<Image>().sprite = Close;
-                    Quests.GetComponent<Image>().sprite = Close;
                     ButtonsBoarder.SetActive(false);
                     swipe.canSwipe = false;
                     SpecialCurrencyTab.SetActive(true);
-                    HealthBar.SetActive(true);
-                    ProgessSwitchButton.SetActive(true);
-                    UpgradeTab.SetActive(false);
-                    QuestTab.SetActive(false);
-                    AscensionTab.SetActive(false);
-                    ExtraAreaTab.SetActive(false);
-                }
-                
-                break;
-            case 4:
-                if (ExtraAreaTab.active)
-                {
-                    Extra.GetComponent<Image>().sprite = Close;
-                    ButtonsBoarder.SetActive(true);
-                    HealthBar.SetActive(true);
-                    ProgessSwitchButton.SetActive(true);
-                    ExtraAreaTab.SetActive(false);
-                    swipe.canSwipe = true;
-                }
-                else
-                {
+                    break;
+                case 4:
                     Extra.GetComponent<Image>().sprite = Open;
-                    Shop.GetComponent<Image>().sprite = Close;
-                    Ascension.GetComponent<Image>().sprite = Close;
-                    GemShop.GetComponent<Image>().sprite = Close;
-                    Quests.GetComponent<Image>().sprite = Close;
                     ButtonsBoarder.SetActive(false);
                     swipe.canSwipe = false;
                     HealthBar.SetActive(false);
                     ProgessSwitchButton.SetActive(false);
                     ExtraAreaTab.SetActive(true);
-                    UpgradeTab.SetActive(false);
-                    QuestTab.SetActive(false);
-                    AscensionTab.SetActive(false);
-                    SpecialCurrencyTab.SetActive(false);
-                }
-                
-                break;
-            case 5:
-                if (activeUpgradePanel == 1)
-                {
-                    UpgradePanel.SetActive(true);
-                    GearPanel.SetActive(false);
-                    swipe.canSwipe = true;
-                }
-                else
-                {
-                    UpgradePanel.SetActive(false);
-                    GearPanel.SetActive(true);
-                    swipe.canSwipe = false;
-                }
-                break;
-            case 6:
-                if (QuestTab.active)
-                {
-                    Quests.GetComponent<Image>().sprite = Close;
-                    ButtonsBoarder.SetActive(true);
-                    QuestTab.SetActive(false);
-                    swipe.canSwipe = true;
-                }
-                else
-                {
+                    break;
+                case 5:
+                    if (activeUpgradePanel == 1)
+                    {
+                        GearTitle.GetComponent<Image>().color = Dark;
+                        UpgradeTitle.GetComponent<Image>().color = Light;
+                        UpgradePanel.SetActive(true);
+                        GearPanel.SetActive(false);
+                        swipe.canSwipe = true;
+                    }
+                    else
+                    {
+                        UpgradeTitle.GetComponent<Image>().color = Dark;
+                        GearTitle.GetComponent<Image>().color = Light;
+                        UpgradePanel.SetActive(false);
+                        GearPanel.SetActive(true);
+                        swipe.canSwipe = false;
+                    }
+                    break;
+                case 6:
                     Quests.GetComponent<Image>().sprite = Open;
-                    Shop.GetComponent<Image>().sprite = Close;
-                    Ascension.GetComponent<Image>().sprite = Close;
-                    GemShop.GetComponent<Image>().sprite = Close;
-                    Extra.GetComponent<Image>().sprite = Close;
                     ButtonsBoarder.SetActive(false);
                     swipe.canSwipe = false;
-                    HealthBar.SetActive(true);
-                    ProgessSwitchButton.SetActive(true);
                     QuestTab.SetActive(true);
-                    UpgradeTab.SetActive(false);
-                    SpecialCurrencyTab.SetActive(false);
-                    AscensionTab.SetActive(false);
-                    ExtraAreaTab.SetActive(false);
-
-                }
-                
-                break;
+                    break;
+            }
         }
     }
 
@@ -205,24 +152,28 @@ public class ShopButtonPopUp : MonoBehaviour
     {
         activeShopTab = 1;
         changeShopTab();
+        lastButton = 1;
     }
 
     public void OpenAscensionTab()
     {
         activeShopTab = 2;
         changeShopTab();
+        lastButton = 2;
     }
 
     public void OpenSpecialCurrencyTab()
     {
         activeShopTab = 3;
         changeShopTab();
+        lastButton = 3;
     }
 
     public void OpenExtrasTab()
     {
         activeShopTab = 4;
         changeShopTab();
+        lastButton = 4;
     }
 
     public void OpenGearTab()
@@ -230,6 +181,7 @@ public class ShopButtonPopUp : MonoBehaviour
         activeUpgradePanel = 2;
         activeShopTab = 5;
         changeShopTab();
+        lastButton = 1;
     }
 
     public void OpenUpgradePanel()
@@ -237,12 +189,13 @@ public class ShopButtonPopUp : MonoBehaviour
         activeUpgradePanel = 1;
         activeShopTab = 5;
         changeShopTab();
+        lastButton = 1;
     }
 
     public void OpenQuestTab()
     {
         activeShopTab = 6;
         changeShopTab();
+        lastButton = 6;
     }
-
 }
