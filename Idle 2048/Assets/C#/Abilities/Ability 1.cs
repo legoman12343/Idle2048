9372fe078a327b0e4ce9c5e3f6f870d2b6aee5c7
legoman12343 <joshua.markey@outlook.com>
2022-05-17 22:47:48 +0100

    notifications

--- Idle 2048/Assets/C#/Abilities/Ability 1.cs
+++ Idle 2048/Assets/C#/Abilities/Ability 1.cs
@@ -14,6 +14,10 @@ public class Ability1 : MonoBehaviour
     public GameObject ability;
     public float sliderMax = 600f;
     private bool bought = false;
+    private bool active = false;
+
+    public GameObject fill;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,30 +25,53 @@ public class Ability1 : MonoBehaviour
         ability.SetActive(false);
     }
 
-    private IEnumerator activateAbility()
+    private IEnumerator cooldownAbility()
     {
-        activeTimer -= Time.deltaTime;
-        activeTimer = 10f;
-        while (activeTimer > 0)
-        {
-            activeTimer -= Time.deltaTime;
-            yield return new WaitForSeconds(0.1f);
-        }
-
+        slider.maxValue = sliderMax;
         cooldownTimer -= Time.deltaTime;
         cooldownTimer = sliderMax;
 
-        while (cooldownTimer > 0)
+        while (cooldownTimer < 600)
         {
-            cooldownTimer -= Time.deltaTime;
+            cooldownTimer += Time.deltaTime;
             slider.value = cooldownTimer;
             yield return new WaitForSeconds(0.5f);
         }
+        active = true;
+        fill.SetActive(false);
     }
+
+    public void pressAbility1()
+    {
+        StartCoroutine(startAbility());
+    }
+
+
+    public IEnumerator startAbility()
+    {
+        if (active)
+        {
+            fill.SetActive(true);
+            slider.maxValue = 10f;
+            active = false;
+            activeTimer -= Time.deltaTime;
+            activeTimer = 10f;
+            slider.value = 10f;
+            while (activeTimer > 0)
+            {
+                slider.value = activeTimer;
+                activeTimer -= Time.deltaTime;
+                yield return new WaitForSeconds(0.1f);
+            }
+            StartCoroutine(cooldownAbility);
+        }
+    }
+
 
     public void buyAbility1()
     {
         ability.SetActive(true);
         bought = true;
+        StartCoroutine(cooldownAbility());
     }
 }
